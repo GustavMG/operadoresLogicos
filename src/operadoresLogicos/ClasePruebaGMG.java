e6b71c19b4160f74d07097e59c9e7f82f25b1ae9
GustavMG <magg950506@gmail.com>
2023-06-14 02:15:23 -0600

    Se agrega archivo.java de prueba

---
+++ src/operadoresLogicos/ClasePruebaGMG.java
@@ -0,0 +1,10 @@
+package operadoresLogicos;
+
+public class ClasePruebaGMG {
+
+	public static void main(String[] args) {
+		System.out.println("Esta clase es un archivo de prueba debido a problemas que hubo para subir repos al cambiar el nombre de rama master a main");
+
+	}
+
+}
